402968a1bea8ba1ae39f7ac161c6fb9aa29359f4
Brokenheart100 <Brokenheart100@BKTV>
2025-11-03 12:06:13 +0800

    美国

--- ChatProjects.AppHost/AppHost.cs
+++ ChatProjects.AppHost/AppHost.cs
@@ -2,24 +2,20 @@
 
 var cache = builder.AddRedis("cache");
 
-// ���һ����Ϊ "postgres-db" �� PostgreSQL ������������Դ��
 var postgresServer = builder.AddPostgres("postgres-db");
 
-// ������ݿ���Դ���Ա���Ҫ�洢�û�����ݺ��罻��ϵ�ķ������á�
 var userDb = postgresServer.AddDatabase("userdb");
 
-// ���һ����Ϊ "mongo-db" �� MongoDB ������������Դ��
 var mongoServer = builder.AddMongoDB("mongo-db");
 
-// ������ݿ���Դ���������¼�洢�������á�
 var chatHistoryDb = mongoServer.AddDatabase("chathistorydb");
 
 var userService = builder.AddProject<Projects.ChatProjects_UserService>("userservice")
     .WithReference(userDb);
-var authService = builder.AddProject<Projects.ChatProjects_AuthService>("authservice")
+var authService = builder.AddProject<Projects.ChatProjects_AuthService>("authservice1")
     .WithReference(userDb);
 
-builder.AddProject<Projects.ChatProjects_GatewayService>("gatewayservice")
+builder.AddProject<Projects.ChatProjects_GatewayService>("gatewayservice1")
     .WithReference(authService)
     .WithReference(userService);
 
